55af86dfe946e477224448ffc0ba04ccac56fcda
Mukultiwari03 <mukultiwari2032004@gmail.com>
2024-11-06 23:45:32 +0530

    done

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const cors = require('cors');
 const { dbconnect } = require('./config/database');
 
 const app = express();
-app.use(cors());
+app.use(cors(
+    origin='*'
+));
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
