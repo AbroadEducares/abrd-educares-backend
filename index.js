473cba6813a6911650568a98d6bfd011c52f4ba9
Mukultiwari03 <mukultiwari2032004@gmail.com>
2024-11-07 14:31:25 +0530

    changes made

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { dbconnect } = require('./config/database');
 
 const app = express();
 
-  app.use(cors());
+app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
