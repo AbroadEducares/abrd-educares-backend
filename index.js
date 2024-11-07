652790e972e0debf2a4f744656c547d774505631
Mukultiwari03 <mukultiwari2032004@gmail.com>
2024-11-07 14:23:35 +0530

    done something

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const cors = require('cors');
 const { dbconnect } = require('./config/database');
 
 const app = express();
-app.use(cors(
-    origin='*'
-));
+const corsOptions = {
+    origin: ['http://your-frontend-domain.com', 'http://localhost:3000'], // Add your frontend URLs here
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  };
+  
+  app.use(cors(corsOptions));
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
